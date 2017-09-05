eaa4206acfbe6f5dffa2e02a2695fd8222ad6181
kelsey langlois and LDMacKrell <ldmackrell@>
2017-09-05 15:49:26 -0700

    Update clock_angle to display angle for non zero minutes.

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -1,18 +1,18 @@
 class CustomTime
   def initialize(hour, minutes)
     @hour = hour
+    if @hour == 12
+      @hour = 0
+    end
     @minutes = minutes
   end
 
   def clock_angle()
-    if @minutes == 0
-      angle = @hour * 30
-      if angle > 180
-        angle = 360 - angle
-      end
-      return angle
-    else
-      return 0
-    end
+    hour_angle = (@hour * 30) + (0.5 * @minutes)
+    minute_angle = 6 * @minutes
+    return (hour_angle - minute_angle).abs
   end
 end
+
+test_time = CustomTime.new(3,15)
+puts(test_time.clock_angle)
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -11,4 +11,9 @@ describe("#clock_angle") do
     time_test = CustomTime.new(3,0)
     expect(time_test.clock_angle()).to(eq(90))
   end
+
+  it("if the minute amount is non-zero, return the hour's angle from 12 o'clock minus the minute's angle from 12 o'clock in degrees") do
+    time_test = CustomTime.new(12,30)
+    expect(time_test.clock_angle()).to(eq(165))
+  end
 end
